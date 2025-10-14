0076f80d32ca7a1044d481b8cd57a9d5219f40fe
jlopezVylaris <jlopez@vylaris.ar>
2025-10-13 23:30:32 -0300

    Resuelto conflicto en router/index.js

--- FacturasMDS/src/router/index.js
+++ FacturasMDS/src/router/index.js
@@ -3,15 +3,13 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
-import PaymentHistoryView from '../views/PaymentHistoryView.vue'
 
 const routes = [
-  // Landing page: redirect to login
-  { path: '/', redirect: '/login' },
+  // Landing page: Auth view
+  { path: '/', component: AuthView },
 
-  // Home moved to /home
+  // Protected routes
   { path: '/home', name: 'Home', component: HomeView },
-  { path: '/payments', name: 'PaymentHistory', component: PaymentHistoryView },
   { path: '/about', name: 'About', component: AboutView },
   { path: '/login', name: 'Login', component: LoginView },
   { path: '/register', name: 'Register', component: RegisterView }
